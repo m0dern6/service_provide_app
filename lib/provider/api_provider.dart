92e7f698c3bb30442c55307c7faaf0a3ab208959
m0dern6 <na.ab.en.153@gmail.com>
2024-04-20 09:49:00 +0545

    removed previous login methods which uses get method and was only applicable with previous model

--- lib/provider/api_provider.dart
+++ lib/provider/api_provider.dart
@@ -1,25 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:service_provide_app/models/category_model.dart';
-import 'package:service_provide_app/models/user_model.dart';
 import 'package:service_provide_app/repositories/api_repository.dart';
 
 class ApiProvider extends ChangeNotifier {
   final ApiRepository _apiRepository = ApiRepository();
 
-  List<UserModel> _users = [];
-  List<UserModel> get users => _users;
-
   List<CategoryModel> _categories = [];
   List<CategoryModel> get categories => _categories;
 
   ApiProvider() {
-    fetchUser();
     fetchCategory();
-  }
-
-  Future<void> fetchUser() async {
-    _users = await _apiRepository.fetchUser();
-    notifyListeners();
   }
 
   Future<void> fetchCategory() async {
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,22 +1,10 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:service_provide_app/models/category_model.dart';
-import 'package:service_provide_app/models/user_model.dart';
 
 class ApiService {
-  static const baseUrl1 = "http://192.168.1.69:3001/users";
-  static const baseUrl2 = "http://192.168.1.69:3002/category";
-
-  Future<List<UserModel>> fetchUser() async {
-    final response = await http.get(Uri.parse(baseUrl1));
-    if (response.statusCode == 200) {
-      final data1 = jsonDecode(response.body) as List;
-      return data1.map((userData) => UserModel.fromJson(userData)).toList();
-    } else {
-      throw Exception('Failed to load users');
-    }
-  }
+  static const baseUrl1 = "http://192.168.1.64:8000/user/";
+  static const baseUrl2 = "http://192.168.1.64:3000/category";
 
   Future<List<CategoryModel>> fetchCategory() async {
     final response = await http.get(Uri.parse(baseUrl2));
