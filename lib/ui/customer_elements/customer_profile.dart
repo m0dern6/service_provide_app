267e4a10812c3a1781fa4190547fb9cdc53749f9
m0dern6 <na.ab.en.153@gmail.com>
2024-04-22 12:48:05 +0545

    added logout function

--- lib/ui/customer_elements/customer_profile.dart
+++ lib/ui/customer_elements/customer_profile.dart
@@ -19,28 +19,155 @@ class _CustomerProfileState extends State<CustomerProfile> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            Column(
-              children: [
-                Row(
-                  children: [CircleAvatar()],
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                ElevatedButton(
-                    onPressed: () {
-                      _logout();
-                    },
-                    child: Text('Logout'))
-              ],
-            ),
-          ],
-        ),
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'assets/logo.png',
+            fit: BoxFit.cover,
+          ),
+          DraggableScrollableSheet(
+            initialChildSize: 0.35,
+            minChildSize: 0.1,
+            builder: (context, controller) {
+              return ListView(
+                children: [
+                  Container(
+                    padding: EdgeInsets.fromLTRB(20, 10, 0, 10),
+                    color: Colors.white,
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.language,
+                                  size: 40,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  'Language',
+                                  style: TextStyle(fontSize: 25),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Icon(Icons.arrow_forward_ios, size: 30),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(20, 2, 20, 2),
+                          child: Divider(
+                            color: Colors.black,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.feedback_outlined,
+                                  size: 40,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  'Help/Feedback',
+                                  style: TextStyle(fontSize: 25),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Icon(Icons.arrow_forward_ios, size: 30),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(20, 2, 20, 2),
+                          child: Divider(
+                            color: Colors.black,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.settings,
+                                  size: 40,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  'Settings',
+                                  style: TextStyle(fontSize: 25),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Icon(Icons.arrow_forward_ios, size: 30),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(20, 2, 20, 2),
+                          child: Divider(
+                            color: Colors.black,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            _logout();
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.logout_rounded,
+                                    size: 40,
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text(
+                                    'Logout',
+                                    style: TextStyle(fontSize: 25),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Icon(Icons.arrow_forward_ios, size: 30),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            },
+          )
+        ],
       ),
     );
   }
